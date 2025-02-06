00d3420cc91ccd4b7dd1705496f5b7636c9b55cf
Keolin Moonsamy <impulse372@gmail.com>
2025-02-06 08:30:41 +0200

    Update Message.cs

--- Models/Message.cs
+++ Models/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Models
 {
+    // Represents a message containing data and its corresponding type.
     public class Message
     {
         public string MessageData { get; set; }
