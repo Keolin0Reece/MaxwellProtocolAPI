75be138a78e2a979882360a078b008a4e764a74a
Keolin Moonsamy <impulse372@gmail.com>
2025-02-06 08:35:24 +0200

    Update ArduinoHub.cs

--- ArduinoHub.cs
+++ ArduinoHub.cs
@@ -5,8 +5,15 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace APIService
 {
+    /// <summary>
+    /// SignalR hub for real-time communication between the server and connected clients.
+    /// </summary>
     public class ArduinoHub : Hub
     {
+        /// <summary>
+        /// Sends data to all connected clients.
+        /// </summary>
+        /// <param name="data">The data to be broadcast.</param>
         public async Task SendData(string data)
         {
             await Clients.All.SendAsync("ReceiveData", data);
